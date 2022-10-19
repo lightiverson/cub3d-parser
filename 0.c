d75b153691ceb4ff5a00d03d7463ae10de453a03
Kawish Gajadien <kawishg@outlook.com>
2022-10-19 19:09:53 +0200

    fix: leaks

--- 0.c
+++ 0.c
@@ -1,8 +1,9 @@
 #include "file_to_str.h"
+#include "libft/libft.h"
 
 typedef struct s_map_element {
 	char					*map_element;
-	size_t					len;
+	int						type;
 	struct s_map_element	*next;
 }	t_map_element;
 
@@ -23,21 +24,24 @@ void print_map_elements(t_map_element *head)
 {
 	while (head)
 	{
-		printf("head->map_element = %.*s\n", (int)head->len, head->map_element);
-		// printf("head->len = %lu\n", head->len);
+		printf("head->map_element = |%s|\n", head->map_element);
+		printf("head->type = %i\n", head->type);
 		head = head->next;
 	}
 }
 
-t_map_element	*init_node(char *map_element, size_t len)
+t_map_element	*init_node(char *map_element, int type)
 {
 	t_map_element *node;
 
 	node = malloc(sizeof(*node));
 	if (!node)
+	{
+		perror("Error: malloc()\n");
 		exit(1);
+	}
 	node->map_element = map_element;
-	node->len = len;
+	node->type = type;
 	node->next = 0;
 	return (node);
 }
@@ -57,114 +61,88 @@ void	map_elements_add_back(t_map_element **lst, t_map_element *new)
 	}
 }
 
-void tokenize_input_elem(char **file_str_dup, t_map_element	**lst)
+void tokenize_input_elem(char *input_elem, int type, t_map_element **lst)
 {
-	size_t len;
-	char			*needle;
-	t_map_element	*leeg;
+	char			*s;
+	t_map_element	*node;
 
-	len = 0;
-	needle = *file_str_dup;
-	while (**file_str_dup != '\n')
+	s = strdup(input_elem);
+	if (!s)
 	{
-		len++;
-		(*file_str_dup)++;
+		perror("Error: malloc()\n");
+		exit(1);
 	}
-	leeg = init_node(needle, len);
-	map_elements_add_back(lst, leeg);
+	node = init_node(s, type);
+	map_elements_add_back(lst, node);
+}
+
+void free_splitted_array(char **splitted_array)
+{
+	int	i;
+
+	i = 0;
+	while (splitted_array[i])
+	{
+		free(splitted_array[i]);
+		i++;
+	}
+	free(splitted_array);
+}
+
+void free_map_elements(t_map_element *head)
+{
+	t_map_element	*tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->map_element);
+		free(tmp);
+	}
 }
 
 int main (int argc, char *argv[argc + 1])
 {
 	int				map_fd;
 	char			*file_str;
-	char			*file_str_dup;
 	t_map_element	*lst;
-	t_map_element	*leeg;
-	char			*needle;
+	char			**a;
+	unsigned int	i;
 
-	map_fd = get_map_fd("map.cub");
+	map_fd = get_map_fd(argv[argc - 1]);
 	file_to_str(map_fd, &file_str);
 
 	printf("file_to_str = |%s|\n", file_str);
 
+	i = 0;
 	lst = 0;
-	unsigned int len = 0;
-	file_str_dup = file_str;
-	while (*file_str_dup)
+	a = ft_split(file_str, '\n');
+	if (!a)
 	{
-		if (!strncmp(file_str_dup, "NO ", 3))
-		{
-			tokenize_input_elem(&file_str_dup, &lst);
-			// needle = file_str_dup;
-			// while (*file_str_dup != '\n')
-			// {
-			// 	len++;
-			// 	file_str_dup++;
-			// }
-			// leeg = init_node(needle, len);
-			// map_elements_add_back(&lst, leeg);
-			// len = 0;
-		}
-	
-		else if (!strncmp(file_str_dup, "SO ", 3))
-		{
-			tokenize_input_elem(&file_str_dup, &lst);
-			// needle = file_str_dup;
-			// while (*file_str_dup != '\n')
-			// {
-			// 	len++;
-			// 	file_str_dup++;
-			// }
-			// leeg = init_node(needle, len);
-			// map_elements_add_back(&lst, leeg);
-			// len = 0;
-		}
-	
-		else if (!strncmp(file_str_dup, "WE ", 3))
-		{
-			tokenize_input_elem(&file_str_dup, &lst);
-			// needle = file_str_dup;
-			// while (*file_str_dup != '\n')
-			// {
-			// 	len++;
-			// 	file_str_dup++;
-			// }
-			// leeg = init_node(needle, len);
-			// map_elements_add_back(&lst, leeg);
-			// len = 0;
-		}
-	
-		else if (!strncmp(file_str_dup, "F ", 2))
-		{
-			tokenize_input_elem(&file_str_dup, &lst);
-			// needle = file_str_dup;
-			// while (*file_str_dup != '\n')
-			// {
-			// 	len++;
-			// 	file_str_dup++;
-			// }
-			// leeg = init_node(needle, len);
-			// map_elements_add_back(&lst, leeg);
-			// len = 0;
-		}
-	
-		else if (!strncmp(file_str_dup, "C ", 2))
-		{
-			tokenize_input_elem(&file_str_dup, &lst);
-			// needle = file_str_dup;
-			// while (*file_str_dup != '\n')
-			// {
-			// 	len++;
-			// 	file_str_dup++;
-			// }
-			// leeg = init_node(needle, len);
-			// map_elements_add_back(&lst, leeg);
-			// len = 0;
-		}
-	
-		file_str_dup++;
+		perror("Error: malloc()\n");
+		exit(1);
 	}
+	while (a[i])
+	{
+		if (!strncmp(a[i], "NO ", 3))
+			tokenize_input_elem(a[i], 0, &lst);
+		else if (!strncmp(a[i], "SO ", 3))
+			tokenize_input_elem(a[i], 0, &lst);
+		else if (!strncmp(a[i], "WE ", 3))
+			tokenize_input_elem(a[i], 0, &lst);
+		else if (!strncmp(a[i], "EA ", 3))
+			tokenize_input_elem(a[i], 0, &lst);
+		else if (!strncmp(a[i], "F ", 2))
+			tokenize_input_elem(a[i], 1, &lst);
+		else if (!strncmp(a[i], "C ", 2))
+			tokenize_input_elem(a[i], 1, &lst);
+		// else
+		// 	printf("Wat moeten we hier doen? Map element handelen?\n");
+		i++;
+	}
+	free(file_str);
+	free_splitted_array(a);
 	print_map_elements(lst);
 	return (0);
 }
